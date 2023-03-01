beea929a80d0646f768d91fa5b3bf980fa7e07ee
YauhenKuntsevich <jauhien.kuncevicz@gmail.com>
2023-03-01 16:11:45 +0300

    Program script

--- RPGTask/Program.cs
+++ RPGTask/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGTask
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            knight.Attack();
+
+            Archer archer = new Archer();
+            archer.Attack();
+
+            Mage mage = new Mage();
+            mage.Attack();
+        }
+    }
+}
